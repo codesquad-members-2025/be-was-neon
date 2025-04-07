feba02afb48ebd9a9730b11de882e78191284359
subin <eltm521@naver.com>
2025-04-07 11:32:39 +0900

    feat : index.html 제공 기능 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,8 +1,14 @@
 package webserver;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 
@@ -23,15 +29,54 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String line = br.readLine();
+            logger.debug("requestLine : {}", line);
+            String[] requestLine = line.split(" ");
+
+            while (!line.isEmpty()) {
+                line = br.readLine();
+                logger.debug("header : {}", line);
+            }
+
+            byte[] body = new byte[0];
+            if (requestLine[0].equals("GET")) {
+                switch (requestLine[1]) {
+                    case "/index.html" -> body = fileToBytes(requestLine[1]);
+                }
+            }
+
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "<h1>Hello World</h1>".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
+
+    private byte[] fileToBytes(String requestUrl){
+        ClassLoader classLoader = RequestHandler.class.getClassLoader();
+        try (InputStream inputStream = classLoader.getResourceAsStream("./static" + requestUrl)) {
+            if (inputStream == null) {
+                throw new FileNotFoundException("파일을 찾을 수 없습니다.");
+            }
+
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+            byte[] data = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = inputStream.read(data, 0, data.length)) != -1) {
+                buffer.write(data, 0, bytesRead);
+            }
+
+            return buffer.toByteArray();
+        } catch (IOException e) {
+            logger.error("error : {}", e.getMessage());
+        }
+        return null;
+    }
+
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
