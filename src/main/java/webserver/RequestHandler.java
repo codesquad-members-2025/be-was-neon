71147090101c6e12b192c45a96db7aa62d630cb6
gladhee <gladhee@icloud.com>
2025-04-07 12:11:35 +0900

    feat(RequestHandler): static file 서빙 로직 자성
    - 정상 출력 200
    - 파일 찾지 못할 시 404
    - 형식이 안 맞을시 400 에러 리턴

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -26,10 +26,32 @@ public class RequestHandler implements Runnable {
             DataOutputStream dos = new DataOutputStream(out);
 
             Map<String, String> headers = parseRequestHeader(br);
+            String requestLine = headers.get("Request-Line");
+            if (requestLine == null || requestLine.isEmpty()) {
+                send400Response(dos);
+                return;
+            }
 
-            byte[] body = "<h1>Hello World</h1>".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            String[] tokens = requestLine.split(" ");
+            if (tokens.length < 2) {
+                send400Response(dos);
+                return;
+            }
+
+            String uri = tokens[1];
+            InputStream fileIn = getClass().getClassLoader().getResourceAsStream("static" + uri);
+            if (fileIn != null) {
+                // 파일을 읽어 응답 본문에 담음
+                byte[] body = fileIn.readAllBytes();
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            } else {
+                logger.error("File not found: static{}", uri);
+                String notFoundHtml = "<h1>404 Not Found</h1>";
+                byte[] notFoundBody = notFoundHtml.getBytes();
+                response404Header(dos, notFoundBody.length);
+                responseBody(dos, notFoundBody);
+            }
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
@@ -76,5 +98,23 @@ public class RequestHandler implements Runnable {
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private void response404Header(DataOutputStream dos, int lengthOfBodyContent) throws IOException {
+        dos.writeBytes("HTTP/1.1 404 Not Found\r\n");
+        dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+        dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+        dos.writeBytes("\r\n");
+    }
+
+    private void send400Response(DataOutputStream dos) throws IOException {
+        String badRequestHtml = "<h1>400 Bad Request</h1>";
+        byte[] badRequestBody = badRequestHtml.getBytes();
+        dos.writeBytes("HTTP/1.1 400 Bad Request\r\n");
+        dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+        dos.writeBytes("Content-Length: " + badRequestBody.length + "\r\n");
+        dos.writeBytes("\r\n");
+        dos.write(badRequestBody);
+        dos.flush();
     }
 }
