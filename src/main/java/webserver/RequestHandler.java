2a59552cec260c94a83fa678220da5ef59a52130
gladhee <gladhee@icloud.com>
2025-04-07 11:49:19 +0900

    feat(RequestHandler): header 파싱 및 출력 로직 작성

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,9 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,14 +22,40 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
             DataOutputStream dos = new DataOutputStream(out);
+
+            Map<String, String> headers = parseRequestHeader(br);
+
             byte[] body = "<h1>Hello World</h1>".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private Map<String, String> parseRequestHeader(BufferedReader br) throws IOException {
+        Map<String, String> headers = new HashMap<>();
+
+        // 요청 라인(예: "GET / HTTP/1.1")을 읽고 저장
+        String requestLine = br.readLine();
+        headers.put("Request-Line", requestLine);
+        logger.debug(requestLine);
+
+        String line;
+        // 요청 헤더를 읽고 저장
+        while ((line = br.readLine()) != null && !line.isEmpty()) {
+            logger.debug(line);
+            int colonIndex = line.indexOf(":");
+            if (colonIndex != -1) {
+                String key = line.substring(0, colonIndex).trim();
+                String value = line.substring(colonIndex + 1).trim();
+                headers.put(key, value);
+            }
+        }
+
+        return headers;
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
