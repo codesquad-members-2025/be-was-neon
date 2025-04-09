ecfddce90a6e56c405108349670c056fd0f5b820
subin <eltm521@naver.com>
2025-04-09 11:35:52 +0900

    refactor : 스레드풀 생성 try 밖으로 이동

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -25,13 +25,14 @@ public class WebServer {
             port = Integer.parseInt(args[0]);
         }
 
+        ExecutorService executor = Executors.newFixedThreadPool(THREAD_COUNT);
+
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
 
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
-            ExecutorService executor = Executors.newFixedThreadPool(THREAD_COUNT);
             while ((connection = listenSocket.accept()) != null) {
                 executor.submit(new RequestHandler(connection, new FileResourceLoader(), new RequestParser(), new ResponseHandler()));
             }
