bf79a716f7cae805d6eb46949ccb6797f51424d4
subin <eltm521@naver.com>
2025-04-10 12:33:03 +0900

    feat : ClasspathResourceLoader 요청 url 디렉토리인 경우 디폴트로 index.html 응답 - 디렉토리 접근 -> 파일 접근 -> 에러처리 순으로 동작

--- src/main/java/webserver/loader/ClasspathResourceLoader.java
+++ src/main/java/webserver/loader/ClasspathResourceLoader.java
@@ -4,24 +4,19 @@ import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import webserver.request.RequestHandler;
 
 public class ClasspathResourceLoader implements ResourceLoader{
-    private static final String PREFIX = "./static";
+    private static final String PREFIX = "static";
     private static final String FILE_NOT_FOUND = "파일을 찾을 수 없습니다.";
     @Override
     public byte[] fileToBytes(String requestUrl) {
-        ClassLoader classLoader = RequestHandler.class.getClassLoader();
-        try (InputStream inputStream = classLoader.getResourceAsStream(PREFIX + requestUrl)) {
-            if (inputStream == null) {
-                throw new FileNotFoundException(FILE_NOT_FOUND);
-            }
+        try (InputStream is = getInputStreamByUrl(requestUrl)) {
 
             ByteArrayOutputStream buffer = new ByteArrayOutputStream();
             byte[] data = new byte[BUFFER_SIZE];
             int bytesRead;
 
-            while ((bytesRead = inputStream.read(data, OFFSET, data.length)) != -1) {
+            while ((bytesRead = is.read(data, OFFSET, data.length)) != -1) {
                 buffer.write(data, OFFSET, bytesRead);
             }
 
@@ -29,5 +24,30 @@ public class ClasspathResourceLoader implements ResourceLoader{
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private InputStream getInputStreamByUrl(String requestUrl) throws FileNotFoundException {
+        ClassLoader classLoader = ClasspathResourceLoader.class.getClassLoader();
+
+        // 디렉토리에 접근했다 생각하고 index.html을 찾음
+        String indexPath = PREFIX + requestUrl;
+        if (!requestUrl.endsWith("/")) {
+            indexPath += "/";
+        }
+        indexPath += "index.html";
+
+        InputStream inputStream = classLoader.getResourceAsStream(indexPath);
+
+        // index.html이 없으면, 파일 직접 요청
+        if (inputStream == null) {
+            String directPath = PREFIX + requestUrl;
+            inputStream = classLoader.getResourceAsStream(directPath);
+        }
+
+        // 그래도 파일이 없으면 에러
+        if (inputStream == null) {
+            throw new FileNotFoundException(FILE_NOT_FOUND + " : " + requestUrl);
+        }
+        return inputStream;
     }
 }
