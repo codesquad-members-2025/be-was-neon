7488eb68b4624ce1e57259cb8c4a4b65b128b1b8
jaehoonchoi <eeoos8840@gmail.com>
2025-04-10 15:55:37 +0900

    Feat: 브라우저 요청을 읽고 요청 path를 추출하는 기능 추가
    - HttpRequestUtils 유틸 클래스를 생성하여 path 추출

--- src/main/java/util/HttpRequestUtils.java
+++ src/main/java/util/HttpRequestUtils.java
@@ -1,20 +1,9 @@
 package util;
 
-import java.io.*;
+public class HttpRequestUtils {
 
-public class Util {
-
-    public static String extractRequestUrl(String requestLine) {
+    public static String extractRequestPath(String requestLine) {
         String[] requestLineArr = requestLine.split(" ");
         return requestLineArr[1];
-    }
-
-    public static byte[] readFileBytes(String relativePath) throws IOException {
-        try (InputStream is = Util.class.getClassLoader().getResourceAsStream("static" + relativePath)) {
-            if (is == null) {
-                throw new FileNotFoundException("Resource not found: static" + relativePath);
-            }
-            return is.readAllBytes();
-        }
     }
 }
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,11 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,6 +21,21 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String line = reader.readLine();
+            logger.debug("request line: {}", line);
+            String path = HttpRequestUtils.extractRequestPath(line);
+            
+
+            logger.debug("request path: {}", path);
+
+            while (!line.isEmpty()) {
+                line = reader.readLine();
+                logger.debug("header: {}", line);
+            }
+
+
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "<h1>Hello World</h1>".getBytes();
@@ -36,13 +49,15 @@ public class RequestHandler implements Runnable {
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n"); // content-type 분기처리 어떻게 할지 고민
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
+
+
 
     private void responseBody(DataOutputStream dos, byte[] body) {
         try {
