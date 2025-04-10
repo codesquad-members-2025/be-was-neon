325b610ffdc3e206c0f7e9fef193fa8acea69ff3
Gyuwon Moon <gyuwon822@gmail.com>
2025-04-10 11:38:10 +0900

    feat: Content-Type 클래스 구현

---
+++ src/main/java/webserver/util/ContentTypeMapper.java
@@ -0,0 +1,14 @@
+package webserver.util;
+
+public class ContentTypeMapper {
+    public static String getContentType(String path) {
+        if (path.endsWith(".html")) return "text/html;charset=utf-8";
+        if (path.endsWith(".css")) return "text/css";
+        if (path.endsWith(".js")) return "application/javascript";
+        if (path.endsWith(".json")) return "application/json";
+        if (path.endsWith(".png")) return "image/png";
+        if (path.endsWith(".jpg") || path.endsWith(".jpeg")) return "image/jpeg";
+        if (path.endsWith(".gif")) return "image/gif";
+        return "application/octet-stream"; // 기본값
+    }
+}
