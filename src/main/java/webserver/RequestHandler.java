bbe006d221dac6b691e44f496e84a6d935ee1136
jkchoi490 <jkchoi490@daum.net>
2025-04-10 15:28:06 +0900

    feat: HTTP Request 내용 출력 (서버로 들어오는 HTTP Request의 내용을 읽고 파싱 & 로거를 이용하여 출력하기)

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,6 +2,7 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.HashMap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,19 +23,33 @@ public class RequestHandler implements Runnable { //
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             BufferedReader br = new BufferedReader(new InputStreamReader(in , "UTF-8"));
             String line = br.readLine();
+            String[] request_lines = line.split(" ");
+            String file = request_lines[1];
+            file = file.replace('/', '\\');
             logger.debug("request line : {}", line);
+
+            //HTTP request 내용 파싱하기
+            HashMap<String, String> requests = new HashMap<>();
+            HashMap<String, String> headers = new HashMap<>();
+            String requestLine = "requestLine";
+            String header = "header";
+            loggerParser(line, requests, requestLine);
             while(!line.isEmpty()){
                 line = br.readLine();
                 logger.debug("header : {}", line);
+                loggerParser(line, headers, header);
             }
+
+            //System.out.println(requests);
+            //System.out.println(headers);
+
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            //1. 파싱
 
-            // 2. url
-            String url = "C:\\CodeSquad-Project-WebServer\\be-was-neon\\src\\main\\resources\\static\\index.html";
+            // index.html 파일 읽어오기
+            String url = "C:\\CodeSquad-Project-WebServer\\be-was-neon\\src\\main\\resources\\static";
+            url += file;
 
-            // 파일 읽어오기
             br = new BufferedReader(new FileReader(url));
             String content = "";
             while(true){
@@ -43,12 +58,31 @@ public class RequestHandler implements Runnable { //
                 if(l == null) break;
             }
 
-            //3. index.html의 경로에 있는 내용들을 body에 담아주기
+            //index.html의 경로에 있는 내용들을 body에 담아주기
             byte[] body = content.getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
+        }
+    }
+
+    private void loggerParser(String line, HashMap<String, String> map, String type) {
+
+        if(type.equals("requestLine")){
+            String[] request_strs = line.split(" ");
+            map.put("METHOD",request_strs[0]);
+            map.put("Directory and FileName",request_strs[1]);
+            map.put("Protocol Version", request_strs[2]);
+        }
+        else if(type.equals("header")){
+            String[] header_strs = line.split(": ");
+            StringBuilder value = new StringBuilder();
+            for(int i = 1; i<header_strs.length; i++){
+                value.append(header_strs[i]);
+            }
+            map.put(header_strs[0], value.toString());
+
         }
     }
 
