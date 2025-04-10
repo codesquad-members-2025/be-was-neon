0361677c495403b45f1fc3f2e86948e0a4d234e3
Gyuwon Moon <gyuwon822@gmail.com>
2025-04-10 11:58:23 +0900

    refactor: 파일 이름 소문자로 변환해서 비교

--- src/main/java/webserver/util/ContentTypeMapper.java
+++ src/main/java/webserver/util/ContentTypeMapper.java
@@ -2,13 +2,18 @@ package webserver.util;
 
 public class ContentTypeMapper {
     public static String getContentType(String path) {
-        if (path.endsWith(".html")) return "text/html;charset=utf-8";
-        if (path.endsWith(".css")) return "text/css";
-        if (path.endsWith(".js")) return "application/javascript";
-        if (path.endsWith(".json")) return "application/json";
-        if (path.endsWith(".png")) return "image/png";
-        if (path.endsWith(".jpg") || path.endsWith(".jpeg")) return "image/jpeg";
-        if (path.endsWith(".gif")) return "image/gif";
+        String lowerPath = path.toLowerCase();
+
+        if (lowerPath.endsWith(".html")) return "text/html;charset=utf-8";
+        if (lowerPath.endsWith(".css")) return "text/css;charset=utf-8";
+        if (lowerPath.endsWith(".js")) return "application/javascript;charset=utf-8";
+        if (lowerPath.endsWith(".json")) return "application/json";
+        if (lowerPath.endsWith(".png")) return "image/png";
+        if (lowerPath.endsWith(".jpg") || path.endsWith(".jpeg")) return "image/jpeg";
+        if (lowerPath.endsWith(".gif")) return "image/gif";
+        if (lowerPath.endsWith(".svg")) return "image/svg+xml";
+        if (lowerPath.endsWith(".ico")) return "image/x-icon";
+        if (lowerPath.endsWith(".xml")) return "application/xml;charset=utf-8";
         return "application/octet-stream"; // 기본값
     }
 }
