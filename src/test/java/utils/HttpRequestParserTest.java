ddac561eb3511efc2d7280e66cb09a753ffa0d72
2jiyong <jdragon2000@naver.com>
2025-04-10 20:27:28 +0900

    test: 테스트 추가

--- src/test/java/utils/HttpRequestParserTest.java
+++ src/test/java/utils/HttpRequestParserTest.java
@@ -6,14 +6,34 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HttpRequestParserTest {
-
-    HttpRequestParser httpRequestParser = new HttpRequestParser();
+    @Test
+    @DisplayName("HTTP 요청의 requestLine을 String 배열로 파싱할 수 있다.")
+    void parse_requestLine() {
+        String request = "GET /index.html HTTP/1.1";
+        String[] requestLineParts = HttpRequestParser.parseRequestLine(request).get();
+        assertThat(requestLineParts[0]).isEqualTo("GET");
+        assertThat(requestLineParts[1]).isEqualTo("/index.html");
+        assertThat(requestLineParts[2]).isEqualTo("HTTP/1.1");
+    }
 
     @Test
-    @DisplayName("HTTP 요청의 헤더를 받아 요청 url을 추출한다.")
-    void parse_header_to_url() {
-        String request = "GET /index.html HTTP/1.1";
-        String url = httpRequestParser.parseUrl(request);
-        assertThat(url).isEqualTo("/index.html");
+    @DisplayName("HTTP 요청의 requestLine의 양식이 잘못되었으면 Optional.empty를 반환한다.")
+    void parse_invalid_requestLine() {
+        String request = "GET /index.html";
+        assertThat(HttpRequestParser.parseRequestLine(request)).isEmpty();
+    }
+
+    @Test
+    @DisplayName("HTTP 요청의 requestHeader 한 줄을 받고, key와 value로 나누고 양 옆의 공백을 제거한 후 반환한다.")
+    void parse_requestHeader() {
+        String requestHeader = "Host: localhost:8080";
+        assertThat(HttpRequestParser.parseRequestHeader(requestHeader).get()).containsExactly("Host", "localhost:8080");
+    }
+
+    @Test
+    @DisplayName("HTTP 요청의 requestHeader의 양식이 잘못되었으면 Optional.empty를 반환한다.")
+    void parse_invalid_requestHeader() {
+        String requestHeader = "Host";
+        assertThat(HttpRequestParser.parseRequestHeader(requestHeader)).isEmpty();
     }
 }
