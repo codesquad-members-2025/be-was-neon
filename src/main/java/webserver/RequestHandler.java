b8945218b0c7741e26b09c8c8eb82bc4ebec6cf4
jang-jinuk <dlsndifn98@gmail.com>
2025-04-10 16:38:28 +0900

    feat: HTTP Request 내용 출력 기능 구현
    - 클라이언트의 HTTP 요청의 헤더 부분만 콘솔에 로그를 남기도록 구현했습니다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,9 +1,6 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 
 import org.slf4j.Logger;
@@ -23,7 +20,22 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                if (line.isEmpty()) {
+                    break;
+                }
+                sb.append(line).append("\r\n");
+            }
+            String header = sb.toString();
+
+            String[] headers = header.split("\r\n");
+
+            logger.debug("Client request header(IP: {}, Port: {})\n{}", connection.getInetAddress(),
+                    connection.getPort(), header);
+
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "<h1>Hello World</h1>".getBytes();
             response200Header(dos, body.length);
