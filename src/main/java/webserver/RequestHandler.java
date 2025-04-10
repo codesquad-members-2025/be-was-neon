4adb11b6bf1d96afa819b5d552c8aaf88c859d6d
Gyuwon Moon <gyuwon822@gmail.com>
2025-04-10 11:36:09 +0900

    feat: html 파일 응답

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,11 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import webserver.util.RequestParser;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,9 +21,21 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            // in(inputstream)을 감싸서 텍스트 데이터를 줄 단위로 읽기
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            //out(outputstream)을 감싸서 데이터를 바이트 단위로 출력
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "<h1>Hello World</h1>".getBytes();
+            // ✅ 요청 경로 파싱
+            String path = RequestParser.parseRequestPath(br);
+            logger.debug("요청 경로: {}", path);
+
+            InputStream resource = getClass().getClassLoader().getResourceAsStream("static" + path);
+            if (resource == null) {
+                send404(dos);
+                return;
+            }
+
+            byte[] body = resource.readAllBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
@@ -52,4 +62,19 @@ public class RequestHandler implements Runnable {
             logger.error(e.getMessage());
         }
     }
+
+    private void send404(OutputStream out) throws IOException {
+        String body = "<h1>404 Not Found</h1>";
+        byte[] bytes = body.getBytes();
+
+        DataOutputStream dos = new DataOutputStream(out);
+        dos.writeBytes("HTTP/1.1 404 Not Found\r\n");
+        dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+        dos.writeBytes("Content-Length: " + bytes.length + "\r\n");
+        dos.writeBytes("\r\n");
+        dos.write(bytes, 0, bytes.length);
+        dos.flush();
+    }
+
+
 }
