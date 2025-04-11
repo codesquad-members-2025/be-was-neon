fd36739259d2ba3ed8412c5204d13f49771f5c52
2jiyong <jdragon2000@naver.com>
2025-04-11 15:59:21 +0900

    refactor: 디스패처에서 핸들러를 enum으로 관리

--- src/main/java/webserver/Dispatcher.java
+++ src/main/java/webserver/Dispatcher.java
@@ -8,13 +8,32 @@ import response.handler.StaticResourceHandler;
 import java.io.IOException;
 
 public class Dispatcher {
-    public static Handler getHandler(RequestHeader requestHeader) throws IOException {
-        Handler handler;
-        if(requestHeader.getPath().startsWith("/user/create")){
-            handler = new CreateUserHandler();
-        } else{
-            handler = new StaticResourceHandler();
+    public enum Route{
+        USER_CREATE("/user/create", new CreateUserHandler());
+
+        private final String path;
+        private final Handler handler;
+
+        Route(String path, Handler handler) {
+            this.path = path;
+            this.handler = handler;
         }
-        return handler;
+
+        public String getPath() {
+            return path;
+        }
+
+        public Handler getHandler() {
+            return handler;
+        }
+    }
+
+    public static Handler getHandler(RequestHeader requestHeader) throws IOException {
+        for (Route route : Route.values()) {
+            if (requestHeader.getPath().startsWith(route.getPath())) {
+                return route.getHandler();
+            }
+        }
+        return new StaticResourceHandler();
     }
 }
