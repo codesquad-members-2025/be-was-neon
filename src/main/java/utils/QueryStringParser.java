ed3a7c8d53ed44bdedb178c937675b84ddc0c8ea
2jiyong <jdragon2000@naver.com>
2025-04-11 14:41:32 +0900

    feat: queryString을 parse하는 클래스 추가

---
+++ src/main/java/utils/QueryStringParser.java
@@ -0,0 +1,22 @@
+package utils;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+public class QueryStringParser {
+    public static Map<String, String> parse(String queryString) {
+        Map<String, String> params = new HashMap<String, String>();
+        String[] queryParams = queryString.split("&");
+        for (String param : queryParams) {
+            String[] keyValue = param.split("=");
+            String key = keyValue[0];
+            String value = keyValue.length > 1
+                    ? URLDecoder.decode(keyValue[1], StandardCharsets.UTF_8).strip()
+                    : "";
+            params.put(key, value);
+        }
+        return params;
+    }
+}
