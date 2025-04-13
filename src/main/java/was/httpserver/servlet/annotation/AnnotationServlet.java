944ce6fae85ed6ad2688ffbbe8efe4d590b1d1a1
dorkem <20233144@gs.cwnu.ac.kr>
2025-04-13 16:26:01 +0900

    :recycle: 최적화
    원래같으면 path가 100개있으면 100번 돌아야해서 O(N) 시간복잡도였는데, Map으로 시작할때 path를 한번에 등록하면서 1:1 매핑 가능

--- src/main/java/was/httpserver/servlet/annotation/AnnotationServlet.java
+++ src/main/java/was/httpserver/servlet/annotation/AnnotationServlet.java
@@ -8,52 +8,71 @@ import was.httpserver.PageNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.List;
 
 public class AnnotationServlet implements HttpServlet {
-    private final List<Object> controllers;
+    private final Map<String, ControllerMethod> pathMap;
 
     public AnnotationServlet(List<Object> controllers) {
-        this.controllers = controllers;
+        this.pathMap = new HashMap<>();
+        initializePathMap(controllers);
+    }
+
+    private void initializePathMap(List<Object> controllers) {
+        for (Object controller : controllers) {
+            for (Method method : controller.getClass().getDeclaredMethods()) {
+                if (method.isAnnotationPresent(Mapping.class)) {
+                    String path = method.getAnnotation(Mapping.class).value();
+                    if (pathMap.containsKey(path)) {
+                        ControllerMethod controllerMethod = pathMap.get(path);
+                        throw new IllegalArgumentException("경로 중복 등록, path=" + path + ", method=" + method + ", 이미 등록된 메서드 입니당! =" + controllerMethod.method);
+                    }
+                    pathMap.put(path, new ControllerMethod(controller, method));
+                }
+            }
+        }
     }
 
     @Override
     public void service(HttpRequest request, HttpResponse response) throws IOException {
         String path = request.getPath();
-        for (Object controller : controllers) {
-            Method[] methods = controller.getClass().getDeclaredMethods();
-            for (Method method : methods) {
-                if (method.isAnnotationPresent(Mapping.class)){
-                    Mapping mapping = method.getAnnotation(Mapping.class);
-                    String value = mapping.value();
-                    if (value.equals(path)){
-                        invoke(request, response, controller, method);
-                        return;
-                    }
-                }
-            }
+        ControllerMethod controllerMethod = pathMap.get(path);
+
+        if (controllerMethod == null) {
+            throw new PageNotFoundException("request=" + path);
         }
-        throw new PageNotFoundException("request=" + path);
+        controllerMethod.invoke(request, response);
     }
 
-    private void invoke(HttpRequest request, HttpResponse response, Object controller, Method method) {
-        Class<?>[] parameterTypes = method.getParameterTypes();
-        Object[] args = new Object[parameterTypes.length];
+    private static class ControllerMethod {
+        private final Object controller;
+        private final Method method;
 
-        for (int i = 0; i < parameterTypes.length; i++) {
-            if (parameterTypes[i] == HttpRequest.class) {
-                args[i] = request;
-            } else if (parameterTypes[i] == HttpResponse.class) {
-                args[i] = response;
-            } else {
-                throw new IllegalArgumentException("무슨 타입을 넣은거지.. " + parameterTypes[i]);
-            }
+        public ControllerMethod(Object controller, Method method) {
+            this.controller = controller;
+            this.method = method;
         }
 
-        try {
-            method.invoke(controller, args);
-        } catch (IllegalAccessException | InvocationTargetException e) {
-            throw new RuntimeException(e);
+        public void invoke(HttpRequest request, HttpResponse response) {
+            Class<?>[] parameterTypes = method.getParameterTypes();
+            Object[] args = new Object[parameterTypes.length];
+
+            for (int i = 0; i < parameterTypes.length; i++) {
+                if (parameterTypes[i] == HttpRequest.class) {
+                    args[i] = request;
+                } else if (parameterTypes[i] == HttpResponse.class) {
+                    args[i] = response;
+                } else {
+                    throw new IllegalArgumentException("무슨 타입을 넣은거지.." + parameterTypes[i]);
+                }
+            }
+            try {
+                method.invoke(controller, args);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
