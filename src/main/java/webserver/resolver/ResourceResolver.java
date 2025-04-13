da4bffd6e3f21d9cda34c98e9aa147de674c4309
gladhee <gladhee@icloud.com>
2025-04-13 17:39:05 +0900

    feat(ResourceResolver): path가 디렉토리인 경우 해당 폴더 내 index.html 반환로직 추가

--- src/main/java/webserver/resolver/ResourceResolver.java
+++ src/main/java/webserver/resolver/ResourceResolver.java
@@ -6,16 +6,20 @@ import webserver.http.common.ContentType;
 import webserver.http.exception.HttpException;
 import webserver.http.request.HttpRequest;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
 
-import static webserver.http.response.HttpStatusCode.NOT_FOUND;
-import static webserver.http.response.HttpStatusCode.UNSUPPORTED_MEDIA_TYPE;
+import static webserver.http.response.HttpStatusCode.*;
 
 public class ResourceResolver implements Resolver {
 
     private static final Logger logger = LoggerFactory.getLogger(ResourceResolver.class);
     private static final String BASE_PATH = "static";
+    private static final String INDEX_FILE = "index.html";
+    private static final String FILE_PROTOCOL = "file";
     private final HttpRequest request;
 
     public ResourceResolver(HttpRequest request) {
@@ -23,23 +27,57 @@ public class ResourceResolver implements Resolver {
     }
 
     @Override
-    public ResolveResponse<byte[]> resolve() throws IOException {
+    public ResolveResponse<byte[]> resolve() {
         String path = request.getRequestLine().getPath();
+        URL resourceUrl = resolveResourceUrl(BASE_PATH + path);
 
-        InputStream fileIn = getClass().getClassLoader().getResourceAsStream(BASE_PATH + path);
-        if (fileIn == null) {
-            logger.error("File not found: static{}", path);
-            throw new HttpException(NOT_FOUND);
-        }
+        byte[] body = readResourceBytes(resourceUrl);
 
-        byte[] body = fileIn.readAllBytes();
         if (!ContentType.matches(path)) {
             logger.error("Unsupported content type for URI: {}", path);
             throw new HttpException(UNSUPPORTED_MEDIA_TYPE);
         }
 
-        ContentType contentType = ContentType.getContentType(path);
+        ContentType contentType = ContentType.getContentType(resourceUrl.toString());
         return ResolveResponse.ok(contentType, body);
+    }
+
+    private URL resolveResourceUrl(String fullPath) {
+        URL resourceUrl = getClass().getClassLoader().getResource(fullPath);
+        if (resourceUrl == null) {
+            logger.error("File not found: {}", fullPath);
+            throw new HttpException(NOT_FOUND);
+        }
+
+        if (resourceUrl.getProtocol().equals(FILE_PROTOCOL)) {
+            try {
+                File file = new File(resourceUrl.toURI());
+                if (file.isDirectory()) {
+                    if (!fullPath.endsWith("/")) {
+                        fullPath += "/";
+                    }
+                    fullPath += INDEX_FILE;
+                    resourceUrl = getClass().getClassLoader().getResource(fullPath);
+                    if (resourceUrl == null) {
+                        logger.error("Index file not found in directory: {}", fullPath);
+                        throw new HttpException(NOT_FOUND);
+                    }
+                }
+            } catch (URISyntaxException e) {
+                logger.error("Invalid URI syntax for resource: {}", e.getMessage());
+                throw new HttpException(NOT_FOUND);
+            }
+        }
+
+        return resourceUrl;
+    }
+
+    private byte[] readResourceBytes(URL resourceUrl) {
+        try (InputStream in = resourceUrl.openStream()) {
+            return in.readAllBytes();
+        } catch (IOException e) {
+            throw new HttpException(INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
