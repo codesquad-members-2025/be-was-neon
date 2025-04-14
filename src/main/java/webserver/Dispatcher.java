ad3cc92c1176f8a0da60d60907344b34943a807c
2jiyong <jdragon2000@naver.com>
2025-04-14 14:03:02 +0900

    feat: Route에 method 추가

--- src/main/java/webserver/Dispatcher.java
+++ src/main/java/webserver/Dispatcher.java
@@ -9,14 +9,20 @@ import java.io.IOException;
 
 public class Dispatcher {
     public enum Route{
-        USER_CREATE("/user/create", new CreateUserHandler());
+        USER_CREATE("post","/user/create", new CreateUserHandler());
 
+        private final String method;
         private final String path;
         private final Handler handler;
 
-        Route(String path, Handler handler) {
+        Route(String method, String path, Handler handler) {
+            this.method = method;
             this.path = path;
             this.handler = handler;
+        }
+
+        public String getMethod() {
+            return method;
         }
 
         public String getPath() {
@@ -30,7 +36,7 @@ public class Dispatcher {
 
     public static Handler getHandler(RequestHeader requestHeader) throws IOException {
         for (Route route : Route.values()) {
-            if (requestHeader.getPath().startsWith(route.getPath())) {
+            if (requestHeader.getPath().equals(route.getMethod()) && requestHeader.getPath().startsWith(route.getPath())) {
                 return route.getHandler();
             }
         }
