dbfabb3f32d6672330a7553e0880db9f07f9a8e8
Gyuwon Moon <gyuwon822@gmail.com>
2025-04-15 12:22:02 +0900

    refactor: InputStream을 try‑with‑resources로 감싸 자동 close() 보장

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,17 +2,15 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import webserver.util.ContentTypeMapper;
-import webserver.util.RequestParser;
+import webserver.http.HttpRequest;
+import webserver.http.HttpResponse;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
-    private Socket connection;
+    private final Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -27,22 +25,22 @@ public class RequestHandler implements Runnable {
             DataOutputStream dos = new DataOutputStream(out); //out(outputstream)을 감싸서 데이터를 바이트 단위로 출력
 
             HttpRequest request = new HttpRequest(br); //HttpRequest 객체 생성
-            String path = resolvePath(RequestParser.parseRequestPath(br)); // 요청 경로 파싱, 기본 경로 처리
+            String path = resolvePath(request.getPath()); // 요청 경로 파싱, 기본 경로 처리
             HttpResponse response = new HttpResponse(dos); //HttpReponse 객체 생성
 
-            InputStream resource = getClass().getClassLoader().getResourceAsStream("static" + path);
-            if (resource == null) {
-                response.send404Response();
-                return;  //이게 잘 이해ㅏㄱ 안가네 rewturn 왜 해줌?
-            }
 
-            byte[] body = resource.readAllBytes();
-            response.send200Response(body, path);
+            try (InputStream resource = getClass().getClassLoader().getResourceAsStream("static" + path)) {
+                if (resource == null) {
+                    response.send404Response();
+                    return;
+                }
+                byte[] body = resource.readAllBytes();
+                response.send200Response(body, path);
+            }
 
             //요청라인과 헤더 출력
             logger.debug("Request Line: {}", request.getRequestLine());
-            String filteredHeaders = RequestParser.extractFilteredHeaders(br);
-            logger.debug(filteredHeaders);
+            logger.debug("Important Headers: {}", request.getImportantHeaders());
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
@@ -55,4 +53,5 @@ public class RequestHandler implements Runnable {
         }
         return path;
     }
+
 }
