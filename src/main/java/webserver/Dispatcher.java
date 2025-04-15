040e343d36adf733f83cd9b313d5f9372ca305dd
subin <eltm521@naver.com>
2025-04-15 16:35:22 +0900

    refactor : MethodResolver 리팩토링 - MethodResolver enum -> class로 변경 - 내부 enum을 사용하는 EnumMap으로 조회

--- src/main/java/webserver/Dispatcher.java
+++ src/main/java/webserver/Dispatcher.java
@@ -16,4 +16,3 @@ public class Dispatcher {
         return MethodResolver.getHandlerByPath(request.getRequestUrl(), method).handle(request);
     }
 }
-//dispatch -> http메소드를 찾고 핸들러를 가져옴 -> httpmethod에 함수형으로 핸들러 실행함수를 가지고 있음 -> methodResolver에서 requestUrl로 핸들러 인스턴스를 가져옴
--- src/main/java/webserver/resolver/MethodResolver.java
+++ src/main/java/webserver/resolver/MethodResolver.java
@@ -3,27 +3,46 @@ package webserver.resolver;
 import handler.CreateUserHandler;
 import handler.Handler;
 import handler.StaticFileHandler;
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.Map;
 import webserver.common.HttpMethod;
 import webserver.loader.FileResourceLoader;
 
-public enum MethodResolver {
-    DEFAULT("", new StaticFileHandler(new FileResourceLoader())),
-    CREATE_USER("/user/create", new CreateUserHandler());
+public class MethodResolver {
+    private static final Handler DEFAULT_HANDLER = new StaticFileHandler(new FileResourceLoader());
+    private static final EnumMap<HttpMethod, Map<String, Handler>> ROUTES = new EnumMap<>(HttpMethod.class);
 
-    private String path;
-    private Handler handler;
-
-    MethodResolver(String path, Handler handler) {
-        this.path = path;
-        this.handler = handler;
-    }
-
-    public static Handler getHandlerByPath(String path, HttpMethod method) {
-        if (method.equals(HttpMethod.GET)) return DEFAULT.handler;
-
-        for (MethodResolver value : MethodResolver.values()) {
-            if (value.path.equals(path)) return value.handler;
+    static {
+        // 초기화
+        for (HttpMethod method : HttpMethod.values()) {
+            ROUTES.put(method, new HashMap<>());
         }
-        return DEFAULT.handler;
+
+        // enum 정의 순회하면서 등록
+        for (HandlerMapping route : HandlerMapping.values()) {
+            ROUTES.get(route.method).put(route.path, route.handler);
+        }
     }
+    public static Handler getHandlerByPath(String path, HttpMethod method) {
+        return ROUTES
+                .getOrDefault(method, new HashMap<>())
+                .getOrDefault(path, DEFAULT_HANDLER);
+    }
+
+    private enum HandlerMapping {
+        DEFAULT(HttpMethod.GET, "", DEFAULT_HANDLER),
+        CREATE_USER(HttpMethod.POST, "/user/create", new CreateUserHandler());
+
+        HttpMethod method;
+        private String path;
+        private Handler handler;
+
+        HandlerMapping(HttpMethod method, String path, Handler handler) {
+            this.method = method;
+            this.path = path;
+            this.handler = handler;
+        }
+    }
+
 }
