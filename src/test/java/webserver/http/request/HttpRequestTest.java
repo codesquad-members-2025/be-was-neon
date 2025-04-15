a96ea40bb09dbe7d3f08b234457dabd2000b3bd2
gladhee <gladhee@icloud.com>
2025-04-15 11:17:32 +0900

    test(request): request 테스트코드 작성

--- src/test/java/webserver/http/request/HttpRequestTest.java
+++ src/test/java/webserver/http/request/HttpRequestTest.java
@@ -2,37 +2,29 @@ package webserver.http.request;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.io.BufferedReader;
-import java.io.StringReader;
+import webserver.http.common.HttpHeaders;
+import webserver.http.common.HttpMethod;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class HttpRequestTest {
 
     @Test
-    @DisplayName("정상 요청 문자열 파싱: 올바른 메서드, URI, 프로토콜 및 헤더를 추출해야 한다.")
-    public void 헤더_정상_파싱_테스트() throws Exception {
+    @DisplayName("HTTP 요청 객체가 올바르게 생성되어야 한다.")
+    void 객체_생성_테스트() {
         // Given
-        String requestStr =
-                """
-                        GET /index.html HTTP/1.1\r
-                        Host: localhost\r
-                        User-Agent: TestAgent\r
-                        \r
-                        """;
-        BufferedReader reader = new BufferedReader(new StringReader(requestStr));
+        String requestLine = "GET /index.html HTTP/1.1";
+        HttpHeaders headers = new HttpHeaders();
+        String body = "Hello, World!";
 
         // When
-        HttpRequest request = new HttpRequest(reader);
+        HttpRequest httpRequest = new HttpRequest(new RequestLine(requestLine), headers, body);
 
         // Then
-        assertThat(request.getHeaders())
-                .containsEntry("Host", "localhost")
-                .containsEntry("User-Agent", "TestAgent");
-        assertThat(request.getRequestLine().getMethod().toString()).isEqualTo("GET");
-        assertThat(request.getRequestLine().getPath()).isEqualTo("/index.html");
-        assertThat(request.getRequestLine().getHttpVersion()).isEqualTo("HTTP/1.1");
+        assertThat(httpRequest.getRequestLine().getMethod()).isEqualTo(HttpMethod.GET);
+        assertThat(httpRequest.getRequestLine().getPath()).isEqualTo("/index.html");
+        assertThat(httpRequest.getHeaders()).isEqualTo(headers);
+        assertThat(httpRequest.getBody()).isEqualTo(body);
     }
 
 }
