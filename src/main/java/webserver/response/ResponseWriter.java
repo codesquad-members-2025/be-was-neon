817cb8dc714b83dee0496d30a729f9b9c37b70ce
doopang24 <wodus4145@naver.com>
2025-04-16 12:11:45 +0900

    feat: response를 만들고 보내는 클래스 생성

---
+++ src/main/java/webserver/response/ResponseWriter.java
@@ -0,0 +1,37 @@
+package webserver.response;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class ResponseWriter {
+
+    private static final Logger logger = LoggerFactory.getLogger(ResponseWriter.class);
+    private final DataOutputStream dos;
+    private final String CRLF = "\r\n";
+
+    public ResponseWriter(OutputStream out) {
+        this.dos = new DataOutputStream(out);
+    }
+
+    public void send200(int lengthOfBodyContent, String contentType, byte[] body) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK " + CRLF);
+            dos.writeBytes("Content-Type: " + contentType + CRLF);
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + CRLF);
+            dos.writeBytes(CRLF);
+            dos.write(body, 0, body.length);
+            dos.flush();
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    public void send302Redirect(String location) throws IOException {
+        dos.writeBytes("HTTP/1.1 302 Found " + CRLF);
+        dos.writeBytes("Location: " + location + CRLF);
+        dos.writeBytes(CRLF);
+    }
+}
