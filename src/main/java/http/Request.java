d70b76e19c8dc638bb4c0adad281ba323c78175f
jang-jinuk <dlsndifn98@gmail.com>
2025-04-16 20:17:47 +0900

    refactor: Request 객체 책임 분리
    - Request는 HTTP 요청 값의 저장, 출력, 조회 역할만 담당하도록 변경
    - RequestHandler는 HTTP 요청 파싱 및 Request 객체 생성 책임을 가지도록 수정

--- src/main/java/http/Request.java
+++ src/main/java/http/Request.java
@@ -3,54 +3,17 @@ package http;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Request {
     private static final Logger logger = LoggerFactory.getLogger(Request.class);
 
-    private InputStream in;
     private Map<String, String> requestLine;
     private Map<String, String> headers;
 
-    public Request(InputStream in) throws IOException {
-        this.in = in;
-        this.requestLine = new LinkedHashMap<>();
-        this.headers = new LinkedHashMap<>();
-        parseRequest();
-    }
-
-    private void parseRequest() throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
-        parseRequestLine(bufferedReader);
-        parseRequestHeaders(bufferedReader);
-    }
-
-    private void parseRequestLine(BufferedReader bufferedReader) throws IOException {
-        String[] requestLineParts = bufferedReader.readLine().split(" ");
-        requestLine.put("method", requestLineParts[0]);
-        requestLine.put("path", requestLineParts[1]);
-        requestLine.put("protocol", requestLineParts[2]);
-    }
-
-    private void parseRequestHeaders(BufferedReader bufferedReader) throws IOException {
-        String line;
-
-        while ((line = bufferedReader.readLine()) != null) {
-
-            if (line.isEmpty()) break;
-
-            String[] headerParts = parseLine(line);
-            headers.put(headerParts[0], headerParts[1]);
-        }
-    }
-
-    private String[] parseLine(String line) {
-        return line.split(":");
+    public Request(Map<String, String> requestLine, Map<String, String> headers) {
+        this.requestLine = requestLine;
+        this.headers = headers;
     }
 
     public String getHeader(String headerName) {
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,34 +1,54 @@
 package webserver;
 
-import java.io.*;
-import java.net.Socket;
-
 import http.Request;
-import http.Response;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-public class RequestHandler implements Runnable {
-    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
-    private Socket connection;
+public class RequestHandler {
+    private InputStream in;
 
-    public RequestHandler(Socket connectionSocket) {
-        this.connection = connectionSocket;
+    public RequestHandler(InputStream in) {
+        this.in = in;
     }
 
-    public void run() {
-        logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
+    public Request handleRequest() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+        Map<String, String> requestLine = parseRequestLine(bufferedReader);
+        Map<String, String> headers = parseRequestHeaders(bufferedReader);
+        return new Request(requestLine, headers);
+    }
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            Request request = new Request(in);
-            request.printRequest();
-            String path = request.getRequestLine("path");
-            Response response = new Response(out, path);
-            response.sendResponse();
-        } catch (IOException e) {
-            logger.error(e.getMessage());
+    private Map<String, String> parseRequestLine(BufferedReader bufferedReader) throws IOException {
+        Map<String, String> requestLine = new LinkedHashMap<>();
+        String[] requestLineParts = bufferedReader.readLine().split(" ");
+        requestLine.put("method", requestLineParts[0]);
+        requestLine.put("path", requestLineParts[1]);
+        requestLine.put("protocol", requestLineParts[2]);
+        return requestLine;
+    }
+
+    private Map<String, String> parseRequestHeaders(BufferedReader bufferedReader) throws IOException {
+        Map<String, String> headers = new LinkedHashMap<>();
+
+        String line;
+
+        while ((line = bufferedReader.readLine()) != null) {
+
+            if (line.isEmpty()) break;
+
+            String[] headerParts = parseLine(line);
+            headers.put(headerParts[0], headerParts[1]);
         }
+
+        return headers;
+    }
+
+    private String[] parseLine(String line) {
+        return line.split(":");
     }
 }
