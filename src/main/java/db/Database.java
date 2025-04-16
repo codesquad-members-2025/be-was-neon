3ce31fdc78f2827cc76a11e9554a9edd0a426cb2
2jiyong <jdragon2000@naver.com>
2025-04-16 12:30:14 +0900

    refactor: 유저 추가 확인하기 위해 로그 추가

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -1,16 +1,21 @@
 package db;
 
 import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import request.RequestReader;
 
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Database {
+    private static final Logger logger = LoggerFactory.getLogger(RequestReader.class);
     private static Map<String, User> users = new HashMap<>();
 
     public static void addUser(User user) {
         users.put(user.getUserId(), user);
+        logger.debug("User added: {}", user);
     }
 
     public static User findUserById(String userId) {
