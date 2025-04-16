29972d152cb86e544fc85f07d1e665afec821f99
jaehoonchoi <eeoos8840@gmail.com>
2025-04-16 12:53:53 +0900

    Feat(HttpResponse): HttpResponse의 필드에 상태 코드, 컨텐츠 타입, 헤더, 바디 필드 추가

--- src/main/java/webserver/HttpResponse.java
+++ src/main/java/webserver/HttpResponse.java
@@ -5,27 +5,63 @@ import org.slf4j.LoggerFactory;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class HttpResponse {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    public static final String CRLF = "\r\n";
+    public static final String HTTP_1_1 = "HTTP/1.1";
 
-    public void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private final DataOutputStream dos;
+    private HttpStatus status;
+    private ContentType contentType;
+    private final Map<String, List<String>> headers = new HashMap<>();
+    private byte[] body;
+
+    public HttpResponse(DataOutputStream dos) {
+        this.dos = dos;
+    }
+
+    public void response200Header(int lengthOfBodyContent) {
         try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n"); // content-type 분기처리 어떻게 할지 고민
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
+            dos.writeBytes(HTTP_1_1 + " 200 OK " + CRLF);
+            dos.writeBytes("Content-Type: text/html;charset=utf-8" + CRLF);
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + CRLF);
+            dos.writeBytes(CRLF);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
 
-    public void responseBody(DataOutputStream dos, byte[] body) {
+    public void responseBody(byte[] body) {
         try {
             dos.write(body, 0, body.length);
             dos.flush();
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    public HttpResponse status(HttpStatus status) {
+        this.status = status;
+        return this;
+    }
+
+    public HttpResponse contentType(ContentType contentType) {
+        this.contentType = contentType;
+        return this;
+    }
+
+    public HttpResponse body(byte[] body) {
+        this.body = body;
+        return this;
+    }
+
+    public HttpResponse headers(String name, String value) {
+        headers.computeIfAbsent(name, k -> new ArrayList<>()).add(value);
+        return this;
     }
 }
