ada54ad8b0217e00ec4773dbbdb7eeaed67e3f4b
jang-jinuk <dlsndifn98@gmail.com>
2025-04-17 16:09:50 +0900

    refactor: handler 클래스 패키징
    - webserver 패키지에 위치했던 handler 클래스들을 handler 패키지로 이동

--- src/main/java/handler/DispatcherHandler.java
+++ src/main/java/handler/DispatcherHandler.java
@@ -1,4 +1,4 @@
-package webserver;
+package handler;
 
 import java.io.*;
 import java.net.Socket;
--- src/main/java/handler/ResponseHandler.java
+++ src/main/java/handler/ResponseHandler.java
@@ -1,4 +1,4 @@
-package webserver;
+package handler;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
