34e8dc449376a5b5cb11b9cbc4754b1700b98583
jaehoonchoi <eeoos8840@gmail.com>
2025-04-17 11:06:41 +0900

    Refactor: 내부 클래스 RequestLine을 record로 변경

--- src/main/java/webserver/request/HttpRequestParser.java
+++ src/main/java/webserver/request/HttpRequestParser.java
@@ -1,8 +1,8 @@
-package utils;
+package webserver.request;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import webserver.HttpRequest;
+import utils.HttpRequestUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
