b13c147f4ee14a09c44fe4d8ecbb3262db7d5b4e
guzzang <991012kim@gmail.com>
2025-04-21 22:06:11 +0900

    Refactor: RequestHandler 역할 별로 메서드 분리
    RequestReader 메서드 추가

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,10 +3,7 @@ package webserver;
 import httpconst.HttpConst;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import request.RequestHeader;
-import request.RequestReader;
-import request.RequestRouter;
-import request.RequestStatusLine;
+import request.*;
 
 import java.io.*;
 import java.net.Socket;
@@ -29,40 +26,20 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
 
-            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-            String statusLine = br.readLine();
-            logger.debug("Request received: {}", statusLine);
+            RequestReader requestReader = new RequestReader(in);
 
-            RequestReader requestReader = new RequestReader();
-            RequestStatusLine requestStatusLine = requestReader.readStatusLine(statusLine);
+            RequestStatusLine requestStatusLine = requestReader.readStatusLine();
+            RequestHeader requestHeader = requestReader.readHeader();
+            String body = requestReader.readBody(requestHeader);
 
-            String request;
-            Map<String, String> headers = new HashMap<>();
-            while((request = br.readLine()) != null && !request.isEmpty()){
-                String[] headersInfo = request.split(HttpConst.COLON_SPACE);
-                headers.put(headersInfo[0], headersInfo[1]);
-                // readheader 메서드 return requestHeader
-                logger.debug("Request received: {}", request);
-            }
-            RequestHeader requestHeader = new RequestHeader(headers);
+            Request request = new Request(requestStatusLine, requestHeader, body);
 
-            // http 바디 파싱
-            String contentLengthValue = requestHeader.getHeaders().get(HttpConst.CONTENT_LENGTH);
-            String bodyString = "";
-
-            if (contentLengthValue != null) {
-                int contentLength = Integer.parseInt(contentLengthValue);
-                char[] body = new char[contentLength];
-                int read = br.read(body, 0, contentLength);
-                bodyString = new String(body, 0, read);
-                logger.debug("Request body: {}", bodyString);
-            }
 
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
 
             // request를 넘겨주기
-            RequestRouter.handle(requestStatusLine, dos);
+            RequestRouter.handle(request, dos);
 
         } catch (IOException e) {
             logger.error(e.getMessage());
