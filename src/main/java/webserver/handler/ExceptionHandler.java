01052276035dfc0a2079269fc5d9f04953062a8a
Gyuwon Moon <gyuwon822@gmail.com>
2025-04-22 20:37:26 +0900

    feat: ExceptionHandler 데코레이터 구현 * 데코레이터 패턴을 적용한 클래스 * 핸들러를 감싸서, HttpException은 상태별 처리하고, 그 외는 500으로 처리

---
+++ src/main/java/webserver/handler/ExceptionHandler.java
@@ -0,0 +1,33 @@
+package webserver.handler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.exception.InternalServerErrorException;
+import webserver.http.HttpException;
+import webserver.http.HttpRequest;
+import webserver.http.HttpResponse;
+
+import java.io.IOException;
+
+public class ExceptionHandler implements Handler {
+    private static final Logger log = LoggerFactory.getLogger(ExceptionHandler.class);
+    private final Handler wrappedHandler;
+
+    public ExceptionHandler(Handler wrappedHandler) {
+        this.wrappedHandler = wrappedHandler;
+    }
+
+    @Override
+    public void handle(HttpRequest req, HttpResponse res) throws IOException {
+        try{
+            //실제 처리 메서드 호출
+            wrappedHandler.handle(req, res);
+        } catch (HttpException he) {
+            he.sendError(res);
+        } catch (Exception e){
+            log.error("Unhandled exception processing {}", req.getRequestLine(), e);
+            new InternalServerErrorException().sendError(res);
+        }
+
+    }
+}
