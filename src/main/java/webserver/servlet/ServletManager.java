d22ebf2da1ef775c7504645284ff66780ab095e9
dorkem <20233144@gs.cwnu.ac.kr>
2025-04-22 21:57:22 +0900

    :recycle: Post로 요청받기
    Get으로 요청하면 에러가 생기도록 구현하였습니다.

--- src/main/java/webserver/servlet/ServletManager.java
+++ src/main/java/webserver/servlet/ServletManager.java
@@ -1,68 +1,33 @@
 package webserver.servlet;
 
-import model.User;
 import model.UserRepository;
 import webserver.http.HttpRequest;
 import webserver.http.HttpResponse;
-
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
 
 public class ServletManager {
-    private final Map<String, HttpServlet> servletMap = new HashMap<>();
-    UserRepository userRepository = new UserRepository();
-
-    public void add(String pathPrefix, HttpServlet servlet) {
-        servletMap.put(pathPrefix, servlet);
-    }
-
+    private final UserRepository userRepository = new UserRepository();
     public void execute(HttpRequest request, HttpResponse response) throws IOException {
-        try {
-            resolve(request, response);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        HttpServlet servlet = resolve(request, response);
+        servlet.service(request, response);
     }
 
-    public void resolve(HttpRequest request, HttpResponse response) throws IOException {
-        String requestPath = request.getPath();
+    public HttpServlet resolve(HttpRequest request, HttpResponse response) throws IOException {
+        String path = request.getPath();
 
-        if (isStaticFile(requestPath)) {
-            serveStaticFile(requestPath, response);
-            return;
-        } else if (request.getPath().contains("/create")) {
-            String userId = request.getParameter("userId");
-            String password = request.getParameter("password");
-            String name = request.getParameter("name");
-            String email = request.getParameter("email");
-            User user = new User(userId, password, name, email);
-            userRepository.save(user);
+        if (isStaticFile(path)) {
+            HttpServlet servlet = new StaticServlet();
+            return servlet;
         }
+
+        if (path.equals("/create")&& request.getMethod().equals("POST")) {
+            HttpServlet servlet = new UserCreateServlet(userRepository);
+            return servlet;
+        }
+        return null;
     }
 
     private boolean isStaticFile(String path) {
         return path.matches(".+\\.(html|css|js|png|jpg|jpeg|svg|ico)$");
-    }
-
-    private void serveStaticFile(String path, HttpResponse response) throws IOException {
-        String cssPath = path.startsWith("/") ? path.substring(1) : path;
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("static/" + cssPath);
-        byte[] content = inputStream.readAllBytes();
-        response.setStatus(200);
-        response.setContentType(getContentType(cssPath));
-        response.writeBody(content);
-    }
-
-    private String getContentType(String fileName) {
-        if (fileName.endsWith(".html")) return "text/html; charset=UTF-8";
-        if (fileName.endsWith(".css")) return "text/css";
-        if (fileName.endsWith(".js")) return "application/javascript";
-        if (fileName.endsWith(".png")) return "image/png";
-        if (fileName.endsWith(".jpg") || fileName.endsWith(".jpeg")) return "image/jpeg";
-        if (fileName.endsWith(".svg")) return "image/svg+xml";
-        if (fileName.endsWith(".ico")) return "image/x-icon";
-        return "application/octet-stream";
     }
 }
