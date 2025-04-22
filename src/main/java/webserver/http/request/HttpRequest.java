914e1c52cab51751994602f8c95899c32abea2e4
jaehoonchoi <eeoos8840@gmail.com>
2025-04-22 09:56:44 +0900

    Feat(HttpRequest): bodyParameters 필드 추가

--- src/main/java/webserver/http/request/HttpRequest.java
+++ src/main/java/webserver/http/request/HttpRequest.java
@@ -7,24 +7,27 @@ import java.util.stream.Collectors;
 
 public class HttpRequest {
 
-    private String method;
-    private String path;
-    private String protocol;
+    private final String method;
+    private final String path;
+    private final String protocol;
     private final Map<String, List<String>> headers;
-    private final Map<String, List<String>> parameters;
-    private String body;
+    private final Map<String, List<String>> queryParameters;
+    private final Map<String, List<String>> bodyParameters;
+    private final String body;
 
     public HttpRequest(String method,
                        String path,
                        String protocol,
                        Map<String, List<String>> headers,
-                       Map<String, List<String>> parameters,
+                       Map<String, List<String>> queryParameters,
+                       Map<String, List<String>> bodyParameters,
                        String body) {
         this.method = method;
         this.path = path;
         this.protocol = protocol;
         this.headers = headers;
-        this.parameters = parameters;
+        this.queryParameters = queryParameters;
+        this.bodyParameters = bodyParameters;
         this.body = body;
     }
 
@@ -32,8 +35,17 @@ public class HttpRequest {
         return path;
     }
 
-    public Map<String, List<String>> getParameters() {
-        Map<String, List<String>> copy = parameters.entrySet().stream()
+    public Map<String, List<String>> getQueryParameters() {
+        Map<String, List<String>> copy = queryParameters.entrySet().stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        e -> Collections.unmodifiableList(e.getValue()) // value인 List를 불변으로
+                ));
+
+        return Collections.unmodifiableMap(copy); // Map을 불변으로
+    }
+    public Map<String, List<String>> getBodyParameters() {
+        Map<String, List<String>> copy = bodyParameters.entrySet().stream()
                 .collect(Collectors.toMap(
                         Map.Entry::getKey,
                         e -> Collections.unmodifiableList(e.getValue()) // value인 List를 불변으로
