d68d0685b32f5ee6014f2c49f8f9b1906397da74
Gyuwon Moon <gyuwon822@gmail.com>
2025-04-22 16:20:19 +0900

    refactor: Handler 인터페이스 시그니처 수정 *  HttpException 을 던질 수 있도록 선언

--- src/main/java/webserver/handler/Handler.java
+++ src/main/java/webserver/handler/Handler.java
@@ -1,10 +1,11 @@
 package webserver.handler;
 
+import webserver.http.HttpException;
 import webserver.http.HttpRequest;
 import webserver.http.HttpResponse;
 
 import java.io.IOException;
 
 public interface Handler {
-    void handle(HttpRequest request, HttpResponse response) throws IOException;
+    void handle(HttpRequest request, HttpResponse response) throws IOException, HttpException;
 }
