2b0c30d3ff38fa4fade9a2854b835e57c605d953
Gyuwon Moon <gyuwon822@gmail.com>
2025-04-23 19:26:02 +0900

    refactor: MethodNotAllowedException 에서 method 인자 제거

--- src/main/java/webserver/exception/MethodNotAllowedException.java
+++ src/main/java/webserver/exception/MethodNotAllowedException.java
@@ -3,7 +3,8 @@ package webserver.exception;
 import webserver.http.HttpException;
 
 public class MethodNotAllowedException extends HttpException {
-    public MethodNotAllowedException(String method) {
+    public MethodNotAllowedException() {
+        //todo reponse header 잘 들어가는지 확인하기
         super(405, "Method Not Allowed", "<h1>405 Method Not Allowed</h1>");
     }
 }
