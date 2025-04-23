b5c44f7832b965387a836edd2133f9717e78fa91
jang-jinuk <dlsndifn98@gmail.com>
2025-04-23 12:25:26 +0900

    feat: HTTP Request 바디 파싱 기능 추가
    - HTTP Request에 바디가 존재할 때, 바디를 파싱하는 기능 구현

--- src/main/java/webserver/http/request/RequestParser.java
+++ src/main/java/webserver/http/request/RequestParser.java
@@ -2,6 +2,7 @@ package webserver.http.request;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -24,13 +25,19 @@ public class RequestParser {
         Map<String, String> requestLine = requestLineParser.parseRequestLine(requestLineParts);
         Map<String, String> headers = parseRequestHeaders();
         Map<String, String> queryMap = new LinkedHashMap<>();
+        Map<String, String> body = new LinkedHashMap<>();
+
+        if (isIncludeBody(headers)) {
+            int contentLength = Integer.parseInt(headers.get("Content-Length"));
+            body = parseRequestBody(contentLength);
+        }
 
         if (requestLineParts[1].contains("?")) {
             int queryStart = requestLineParts[1].indexOf('?');
             queryMap = queryParser.parseQuery(requestLineParts[1].substring(queryStart + 1));
         }
 
-        return new Request(requestLine, headers, queryMap);
+        return new Request(requestLine, headers, body, queryMap);
     }
 
     private Map<String, String> parseRequestHeaders() throws IOException {
@@ -40,10 +47,33 @@ public class RequestParser {
 
         while (!(line = readLine()).isEmpty()) {
             String[] headerParts = parseLine(line);
-            headers.put(headerParts[0], headerParts[1]);
+            headers.put(headerParts[0], headerParts[1].trim());
         }
 
         return headers;
+    }
+
+    private Map<String, String> parseRequestBody(int contentLength) throws IOException {
+        Map<String, String> body;
+
+        byte[] readBody = readRequestBody(contentLength);
+
+        String bodyString = new String(readBody, StandardCharsets.UTF_8);
+        body = queryParser.parseQuery(bodyString);
+        return body;
+    }
+
+    private byte[] readRequestBody(int contentLength) throws IOException {
+        byte[] body = new byte[contentLength];
+
+        int byteRead = 0;
+        while (byteRead < contentLength) {
+            int read = in.read(body, 0, contentLength);
+            if (read == -1) break;
+            byteRead += read;
+        }
+
+        return body;
     }
 
     private static String[] parseLine(String line) {
@@ -51,13 +81,17 @@ public class RequestParser {
     }
 
     private String readLine() throws IOException {
-        StringBuilder sb = new StringBuilder();
-        int c;
-        while ((c = in.read()) != -1) {
-            if (c == '\r') continue;
-            if (c == '\n') break;
-            sb.append((char) c);
+        StringBuilder lineBuilder = new StringBuilder();
+        int readChar;
+        while ((readChar = in.read()) != -1) {
+            if (readChar == '\r') continue;
+            if (readChar == '\n') break;
+            lineBuilder.append((char) readChar);
         }
-        return sb.toString();
+        return lineBuilder.toString();
+    }
+
+    private boolean isIncludeBody(Map<String, String> headers) {
+        return headers.containsKey("Content-Length");
     }
 }
