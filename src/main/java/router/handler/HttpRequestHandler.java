5633036a1fc90c42bfc95c297c1a100e054bd997
jaehoonchoi <eeoos8840@gmail.com>
2025-04-23 12:29:30 +0900

    Refactor: 정적 메서드로 구현되어 있던 핸들러를 각각의 클래스로 분리

--- src/main/java/router/handler/HttpRequestHandler.java
+++ src/main/java/router/handler/HttpRequestHandler.java
@@ -1,2 +1,10 @@
-package router.handler;public interface HttpRequestHandler {
+package router.handler;
+
+import webserver.http.request.HttpRequest;
+import webserver.http.response.HttpResponse;
+
+import java.io.IOException;
+
+public interface HttpRequestHandler {
+    void handle(HttpRequest request, HttpResponse response) throws IOException;
 }
