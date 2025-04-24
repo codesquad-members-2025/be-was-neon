532451f15b690ecea4febb86d32c7770da536d09
subin <eltm521@naver.com>
2025-04-24 12:09:59 +0900

    feat : 글 작성자 이름 출력

--- src/main/java/template/ArticleContentRenderer.java
+++ src/main/java/template/ArticleContentRenderer.java
@@ -8,12 +8,23 @@ import model.User;
 
 public class ArticleContentRenderer implements TemplateRenderer {
     private static final String CONTENT_PLACEHOLDER = "{{article_content}}";
+    private static final String AUTHOR_PLACEHOLDER = "{{article_author}}";
 
     @Override
     public byte[] render(User user, byte[] template) {
         String html = new String(template, StandardCharsets.UTF_8);
         Article article = Database.findAllArticles().reversed().getFirst();
-        String content = """
+        String content = articleContentToHtml(article);
+
+        String author = article.getAuthor().getName();
+
+        String replaced = html.replace(CONTENT_PLACEHOLDER, content).replace(AUTHOR_PLACEHOLDER, author);
+        return replaced.getBytes(StandardCharsets.UTF_8);
+
+    }
+
+    private String articleContentToHtml(Article article) {
+        return """
                 <p class="post__title">""" +
                 article.getTitle() + """
                   </p>
@@ -21,10 +32,6 @@ public class ArticleContentRenderer implements TemplateRenderer {
                 article.getContent() + """
                     </p>
                     """;
-
-        String replaced = html.replace(CONTENT_PLACEHOLDER, content);
-        return replaced.getBytes(StandardCharsets.UTF_8);
-
     }
 
 
