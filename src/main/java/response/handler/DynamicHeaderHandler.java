0ae1b12c5a3d164162cd0378cfa9ccd855b42e1a
2jiyong <jdragon2000@naver.com>
2025-04-24 22:18:05 +0900

    refactor: 요청에서 세션 ID로 로그인 한 유저를 찾는 로직 분리

--- src/main/java/response/handler/DynamicHeaderHandler.java
+++ src/main/java/response/handler/DynamicHeaderHandler.java
@@ -1,12 +1,15 @@
 package response.handler;
 
+import model.User;
 import request.Request;
 import response.Response;
 import response.ResponseSender;
 import response.Status;
+import session.SessionUserResolver;
 import templates.HeaderModelBuilder;
 import templates.TemplateEngine;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import static constants.HttpHeaders.*;
@@ -17,7 +20,11 @@ public class DynamicHeaderHandler implements Handler{
     public void sendResponse(Request request, ResponseSender responseSender) {
         String path = request.getRequestHeader().getPath();
 
-        Map<String,String> model = HeaderModelBuilder.build(request);
+        Map<String, String> model = new HashMap<>();
+
+        User user = SessionUserResolver.getSessionUserFromRequest(request).orElse(null);
+
+        HeaderModelBuilder.build(model, user);
 
         byte[] body = TemplateEngine.render(path, model);
 
--- src/main/java/templates/HeaderModelBuilder.java
+++ src/main/java/templates/HeaderModelBuilder.java
@@ -1,33 +1,19 @@
 package templates;
 
-import request.Request;
-import session.Session;
-import session.SessionManager;
-import utils.RequestHeaderValueParser;
+import model.User;
 
-import java.util.HashMap;
 import java.util.Map;
 
-import static constants.HttpHeaders.COOKIE;
-import static constants.HttpValues.SESSIONID;
-import static constants.SpecialChars.*;
-
 public class HeaderModelBuilder {
-    public static Map<String,String> build(Request request){
-        Map<String,String> model = new HashMap<>();
+    public static Map<String, String> build(Map<String,String> model, User user) {
+        if (user != null) {
+            model.put("nickname", HtmlBuilder.headerNickname(user.getNickname()));
+            model.put("loginOrLogout", HtmlBuilder.headerLogoutButton());
+        } else {
+            model.put("nickname", "");
+            model.put("loginOrLogout", HtmlBuilder.headerLoginButton());
+        }
 
-        request.getRequestHeader().getHeaderByKey(COOKIE)
-                .map(header -> RequestHeaderValueParser.parseKeyValuePairs(header, SEMICOLON))
-                .map(pairs -> RequestHeaderValueParser.getValueByKey(pairs, SESSIONID, EQUALS))
-                .flatMap(SessionManager::getSession)
-                .map(Session::getUser)
-                .ifPresentOrElse(user -> {
-                            model.put("nickname", HtmlBuilder.headerNickname(user.getNickname()));
-                            model.put("loginOrLogout", HtmlBuilder.headerLogoutButton());},
-                        () -> {
-                            model.put("nickname", EMPTY);
-                            model.put("loginOrLogout", HtmlBuilder.headerLoginButton());
-                        });
         return model;
     }
 }
