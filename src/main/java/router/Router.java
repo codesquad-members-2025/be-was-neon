1cf6615fadc7c90a8bebfd683f3cdb552e91ec3c
jaehoonchoi <eeoos8840@gmail.com>
2025-04-25 11:31:39 +0900

    Refactor: enum 구조 변경
    - enum 안에 메서드, 경로, 핸들러를 속성으로 넣는 방식에서 Map<메서드, Map<경로, 핸들러>> enumMap 구조로 수정

--- src/main/java/router/Router.java
+++ src/main/java/router/Router.java
@@ -4,17 +4,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import router.handler.HttpRequestHandler;
 import router.handler.impl.StaticFileHandler;
+import webserver.common.HttpMethod;
 import webserver.http.request.HttpRequest;
 import webserver.http.response.HttpResponse;
 
 import java.io.IOException;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Router {
+
     private static final Logger logger = LoggerFactory.getLogger(Router.class);
+    private final RouteRegistry routeRegistry;
     private final HttpRequestHandler staticFileHandler;
 
     public Router() {
+        this.routeRegistry = RouteRegistry.getInstance();
         this.staticFileHandler = new StaticFileHandler();
     }
 
@@ -23,17 +28,17 @@ public class Router {
         String method = request.getMethod();
 
         try {
-            if (RequestRoute.ALLOWED_METHODS.containsKey(path)) {
-                if (!RequestRoute.isMethodAllowed(method, path)) {
-                    Set<String> allowedMethods = RequestRoute.getAllowedMethods(path);
-                    String allowHeader = String.join(", ", allowedMethods);
-                    response.send405(allowHeader);
+            if (routeRegistry.hasRoutesForPath(path)) {
+                if (!routeRegistry.isMethodAllowed(method, path)) {
+                    Set<HttpMethod> allowedMethods = routeRegistry.getAllowedMethods(path);
+                    String allowHeader = allowedMethods.stream().map(Enum::name).collect(Collectors.joining(", "));
+                    response.send405(allowHeader); //todo errorHandler로 처리하게 변경
                     return;
                 }
 
-                RequestRoute route = RequestRoute.findByMethodAndPath(method, path);
-                if (route != null) {
-                    route.getHandler().handle(request, response);
+                HttpRequestHandler handler = routeRegistry.findHandler(method, path);
+                if (handler != null) {
+                    handler.handle(request, response);
                     return;
                 }
             }
