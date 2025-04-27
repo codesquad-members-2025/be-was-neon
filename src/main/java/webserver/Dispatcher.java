84d0e1049fabb78d62b64f9a57075fa5945ce288
2jiyong <jdragon2000@naver.com>
2025-04-28 00:19:07 +0900

    feat: 정확한 path일 때와, prefix일 때를 구분해서 dispatch하도록 수정

--- src/main/java/webserver/Dispatcher.java
+++ src/main/java/webserver/Dispatcher.java
@@ -7,22 +7,28 @@ import java.io.IOException;
 
 public class Dispatcher {
     public enum Route{
-        USER_CREATE("POST","/user/create", new CreateUserHandler()),
-        USER_LOGIN("POST","/user/login", new LoginHandler()),
-        USER_LOGOUT("POST","/user/logout", new LogoutHandler()),
-        MAIN_PAGE("GET", "/index.html", new DynamicHeaderHandler()),
-        DEFAULT_PAGE("GET", "/", new DynamicHeaderHandler()),
-        USER_LIST("GET", "/user/list.html", new UserListHandler()),
-        ARTICLE_FORM("GET", "/article/write.html", new AuthenticationHandler());
+        USER_CREATE("POST","/user/create", new CreateUserHandler(), RouteType.EXACT),
+        USER_LOGIN("POST","/user/login", new LoginHandler(), RouteType.EXACT),
+        USER_LOGOUT("POST","/user/logout", new LogoutHandler(), RouteType.EXACT),
+        MAIN_PAGE("GET", "/index.html", new DynamicHeaderHandler(), RouteType.EXACT),
+        DEFAULT_PAGE("GET", "/", new DynamicHeaderHandler(), RouteType.EXACT),
+        USER_LIST("GET", "/user/list.html", new UserListHandler(), RouteType.EXACT),
+        ARTICLE_FORM("GET", "/article/write.html", new AuthenticationHandler(), RouteType.EXACT);
+
+        public enum RouteType{
+            EXACT, PREFIX;
+        }
 
         private final String method;
         private final String path;
         private final Handler handler;
+        private final RouteType routeType;
 
-        Route(String method, String path, Handler handler) {
+        Route(String method, String path, Handler handler, RouteType routeType) {
             this.method = method;
             this.path = path;
             this.handler = handler;
+            this.routeType = routeType;
         }
 
         public String getMethod() {
@@ -36,12 +42,22 @@ public class Dispatcher {
         public Handler getHandler() {
             return handler;
         }
+
+        public RouteType getRouteType() {
+            return routeType;
+        }
     }
 
     public static Handler getHandler(RequestHeader requestHeader) throws IOException {
         for (Route route : Route.values()) {
-            if (requestHeader.getMethod().equals(route.getMethod()) && requestHeader.getPath().equals(route.getPath())) {
-                return route.getHandler();
+            if(route.getRouteType() == Route.RouteType.EXACT) {
+                if (requestHeader.getMethod().equals(route.getMethod()) && requestHeader.getPath().equals(route.getPath())) {
+                    return route.getHandler();
+                }
+            } else {
+                if (requestHeader.getMethod().equals(route.getMethod()) && requestHeader.getPath().startsWith(route.getPath())) {
+                    return route.getHandler();
+                }
             }
         }
         return new StaticResourceHandler();
